60f20435bd4f86e282ab59635e48c93dc9f034e4
Sejun Choi <chltpwns5801@gmail.com>
2024-04-04 15:56:20 +0900

    new branch

--- movieadmin/src/main/java/com/project/movieadmin/HomeController.java
+++ movieadmin/src/main/java/com/project/movieadmin/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome asdasdhome! The client locale is {}.", locale);
+		logger.info("Welcome dasdasdasdhome! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
