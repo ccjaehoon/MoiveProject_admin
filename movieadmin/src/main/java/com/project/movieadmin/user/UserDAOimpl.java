bb2f8d51dd1eec6b1415d5cea200c796ec9a3fcd
JaeHoon Choi <shuu1972@naver.com>
2024-04-05 14:16:20 +0900

    User 추가

--- movieadmin/src/main/java/com/project/movieadmin/user/UserDAOimpl.java
+++ movieadmin/src/main/java/com/project/movieadmin/user/UserDAOimpl.java
@@ -1,5 +1,66 @@
 package com.project.movieadmin.user;
 
-public class UserDAOimpl implements UserDAO {
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+public class UserDAOimpl implements UserDAO {@Override
+	public int u_insert(UserVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int u_update(UserVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int u_delete(UserVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public UserVO u_selectOne(UserVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserVO u_login(UserVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserVO> u_selectAll(int cpage, int pageBlock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserVO> u_searchList(String searchKey, String searchWord, int cpage, int pageBlock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int u_getTotalRows() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int u_getSearchTotalRows(String searchKey, String searchWord) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- movieadmin/src/main/java/com/project/movieadmin/user/UserVO.java
+++ movieadmin/src/main/java/com/project/movieadmin/user/UserVO.java
@@ -1,5 +1,21 @@
 package com.project.movieadmin.user;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class UserVO {
+	
+	private int user_num;
+	private String user_id;
+	private String password;
+	private String nickname;
+	private String authority;
+	private String gender;
+	private Date birth;
+	private String tel;
+	private String favorite;
+	private String email;
 
 }
