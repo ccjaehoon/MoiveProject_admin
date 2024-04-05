622beec31df712c61efca52c2ec5e2069e67cd79
HandCode <34983498@naver.com>
2024-04-05 14:30:47 +0900

    board

--- movieadmin/src/main/java/com/project/movieadmin/board/BoardController.java
+++ movieadmin/src/main/java/com/project/movieadmin/board/BoardController.java
@@ -2,29 +2,40 @@ package com.project.movieadmin.board;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
+@Slf4j
 @Controller
 public class BoardController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	@Autowired
+	private BoardService service;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private HttpSession session;
+
+	@Autowired
+	private ServletContext sContext;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		log.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -34,6 +45,104 @@ public class BoardController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/b_insert.do", method = RequestMethod.GET)
+	public String b_insert() {
+		log.info("Welcome b_insert.do...");
+		
+		return "board/insert";
+	}
+
+	@RequestMapping(value = "/b_insertOK.do", method = RequestMethod.GET)
+	public String b_insertOK(BoardVO vo) {
+		log.info("Welcome b_insertOK.do...");
+		
+		int result = service.b_insert(vo);
+		
+		if (result == 1) {
+			return "redirect:board_selectAll.do";
+		} else {
+			return "redirect:board_insert.do";
+		}
+	}
+	@RequestMapping(value = "/b_selectAll.do", method = RequestMethod.GET)
+	public String b_selectAll(@RequestParam(
+			defaultValue = "1") int cpage,
+			@RequestParam(defaultValue = "5") int pageBlock, Model model) {
+		log.info("Welcome b_selectAll.do...");
+
+		List<BoardVO> vos = service.b_selectAll(cpage, pageBlock);
+
+		model.addAttribute("vos", vos);
+		
+		return "board/selectAll";
+	}
+	
+	@RequestMapping(value = "/b_searchList.do", method = RequestMethod.GET)
+	public String b_searchList(
+			@RequestParam(defaultValue = "1") int cpage,
+			@RequestParam(defaultValue = "5") int pageBlock,
+			Model model, String searchKey, String searchWord) {
+		log.info("Welcome b_searchList.do...");
+		
+		List<BoardVO> vos = service.b_searchList(searchKey, searchWord,cpage,pageBlock);
+		
+		return "board/searchList";
+	}
+	
+	@RequestMapping(value = "/b_selectOne.do", method = RequestMethod.GET)
+	public String b_selectOne(BoardVO vo, Model model) {
+		log.info("Welcome b_insert.do...");
+		
+		BoardVO vo2 = service.b_selectOne(vo);
+		
+		return "board/selectOne";
+	}
+	
+	@RequestMapping(value = "/b_update.do", method = RequestMethod.GET)
+	public String b_update(BoardVO vo, Model model) {
+		log.info("Welcome b_update.do...");
+		
+		BoardVO vo2 = service.b_selectOne(vo);
+
+		model.addAttribute("vo2", vo2);
+		
+		return "board/update";
+	}
+	
+	@RequestMapping(value = "/b_updateOK.do", method = RequestMethod.GET)
+	public String b_updateOK(BoardVO vo) {
+		log.info("Welcome b_updateOK.do...");
+		
+		int result = service.b_update(vo);
+		
+		return "board/update";
+	}
+	
+	@RequestMapping(value = "/b_delete.do", method = RequestMethod.GET)
+	public String b_delete() {
+		log.info("Welcome b_delete.do...");
+		
+		return "board/delete";
+	}
+	
+	@RequestMapping(value = "/b_deleteOK.do", method = RequestMethod.GET)
+	public String b_deleteOK(BoardVO vo) {
+		log.info("Welcome b_deleteOK.do...");
+		log.info("vo:{}", vo);
+
+		int result = service.b_delete(vo);
+		log.info("result:{}", result);
+
+		return "board/delete";
+	}
+	
+	@RequestMapping(value = "/b_increaseGood.do", method = RequestMethod.GET)
+	public String b_increaseGood(BoardVO vo) {
+		log.info("Welcome b_increaseGood.do...");
+		
+		return "board/increaseGood";
 	}
 	
 }
