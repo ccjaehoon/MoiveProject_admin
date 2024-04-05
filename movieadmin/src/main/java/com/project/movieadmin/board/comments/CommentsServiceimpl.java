84aa416ae78e583cc1874ae84daac3e0c6409cdf
HandCode <34983498@naver.com>
2024-04-05 17:18:08 +0900

    BoardServiceimpl, CommentsDAOimpl, CommentsServiceimpl

--- movieadmin/src/main/java/com/project/movieadmin/board/comments/CommentsServiceimpl.java
+++ movieadmin/src/main/java/com/project/movieadmin/board/comments/CommentsServiceimpl.java
@@ -1,31 +1,62 @@
 package com.project.movieadmin.board.comments;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
 public class CommentsServiceimpl implements CommentsService {
+
+	@Autowired
+	private SqlSession sqlSession;
 
 	@Override
 	public int c_insert(CommentsVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		log.info("c_insert()...");
+
+		int flag = sqlSession.insert("INSERT", vo);
+
+		return flag;
 	}
 
 	@Override
 	public int c_update(CommentsVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		log.info("c_update()...");
+
+		int flag = sqlSession.update("UPDATE", vo);
+
+		return flag;
 	}
 
 	@Override
 	public int c_delete(CommentsVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		log.info("c_delete()....");
+
+		int flag = sqlSession.delete("DELETE", vo);
+
+		return flag;
 	}
 
 	@Override
 	public List<CommentsVO> c_selectAll(int cpage, int pageBlock) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("c_selectAll()....");
+
+		int startRow = (cpage - 1) * pageBlock + 1;
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("startRow", startRow - 1);
+		map.put("pageBlock", pageBlock);
+
+		List<CommentsVO> vos = sqlSession.selectList("SELECT_ALL_PAGE_BLOCK", map);
+
+		return vos;
 	}
 
 	@Override
