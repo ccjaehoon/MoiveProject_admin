b0706b598b4cbdf3626ec25a975cad428f8fab5b
HandCode <34983498@naver.com>
2024-04-05 16:39:44 +0900

    Board, Comments

--- movieadmin/src/main/java/com/project/movieadmin/board/BoardController.java
+++ movieadmin/src/main/java/com/project/movieadmin/board/BoardController.java
@@ -10,18 +10,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
+@Slf4j
 @Controller
 public class BoardController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -34,6 +35,80 @@ public class BoardController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "b_insert", method = RequestMethod.GET)
+	public String b_insert() {
+		logger.info("Welcome b_insert.do...");
+		
+		return "board/insert";
+	}
+
+	@RequestMapping(value = "b_insertOK", method = RequestMethod.GET)
+	public String b_insertOK(BoardVO vo) {
+		logger.info("Welcome b_insertOK.do...");
+		
+		return "board/insertOK";
+	}
+	@RequestMapping(value = "b_selectAll", method = RequestMethod.GET)
+	public String b_selectAll(@RequestParam(
+			defaultValue = "1") int cpage,
+			@RequestParam(defaultValue = "5") int pageBlock, Model model) {
+		logger.info("Welcome b_selectAll.do...");
+		
+		return "board/selectAll";
+	}
+	
+	@RequestMapping(value = "b_searchList", method = RequestMethod.GET)
+	public String b_searchList(
+			@RequestParam(defaultValue = "1") int cpage,
+			@RequestParam(defaultValue = "5") int pageBlock,
+			Model model, String searchKey, String searchWord) {
+		logger.info("Welcome b_searchList.do...");
+		
+		return "board/searchList";
+	}
+	
+	@RequestMapping(value = "b_selectOne", method = RequestMethod.GET)
+	public String b_selectOne(BoardVO vo, Model model) {
+		logger.info("Welcome b_insert.do...");
+		
+		return "board/selectOne";
+	}
+	
+	@RequestMapping(value = "b_update", method = RequestMethod.GET)
+	public String b_update(BoardVO vo, Model model) {
+		logger.info("Welcome b_update.do...");
+		
+		return "board/update";
+	}
+	
+	@RequestMapping(value = "b_updateOK", method = RequestMethod.GET)
+	public String b_updateOK(BoardVO vo) {
+		logger.info("Welcome b_updateOK.do...");
+		
+		return "board/updateOK";
+	}
+	
+	@RequestMapping(value = "b_delete", method = RequestMethod.GET)
+	public String b_delete() {
+		logger.info("Welcome b_delete.do...");
+		
+		return "board/delete";
+	}
+	
+	@RequestMapping(value = "b_deleteOK", method = RequestMethod.GET)
+	public String b_deleteOK(BoardVO vo) {
+		logger.info("Welcome b_deleteOK.do...");
+		
+		return "board/deleteOK";
+	}
+	
+	@RequestMapping(value = "b_increaseGood", method = RequestMethod.GET)
+	public String b_increaseGood(BoardVO vo) {
+		logger.info("Welcome b_increaseGood.do...");
+		
+		return "board/increaseGood";
 	}
 	
 }
--- movieadmin/src/main/java/com/project/movieadmin/board/BoardVO.java
+++ movieadmin/src/main/java/com/project/movieadmin/board/BoardVO.java
@@ -1,5 +1,22 @@
 package com.project.movieadmin.board;
 
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
 public class BoardVO {
+	
+	private int board_num;
+	private String title;
+	private String tag;
+	private String nickname;
+	private String content;
+	private Date wdate;
+	private int good;
+	private String authority;
+	private int views;
+	private int commentscount;
+	private String report;
 
 }
