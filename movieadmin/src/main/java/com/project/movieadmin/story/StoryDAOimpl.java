683e98685641d0dfe682e0d986d29ffbe9e97060
(Seyoung Choi) <(c91sy@naver.com)>
2024-04-08 18:48:40 +0900

    스토리, 스토리댓글 imp 편집

--- movieadmin/src/main/java/com/project/movieadmin/story/StoryDAOimpl.java
+++ movieadmin/src/main/java/com/project/movieadmin/story/StoryDAOimpl.java
@@ -2,7 +2,10 @@ package com.project.movieadmin.story;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -22,7 +25,7 @@ public class StoryDAOimpl implements StoryDAO {
 	
 	@Override
 	public int s_insert(StoryVO vo) {
-		log.info("insert()....");
+		log.info("s_insert()....");
 		log.info(vo.toString());
 
 		return sqlSession.insert("S_INSERT", vo);
@@ -30,51 +33,50 @@ public class StoryDAOimpl implements StoryDAO {
 
 	@Override
 	public int s_update(StoryVO vo) {
-		System.out.println("update()....");
-		System.out.println(vo);
-		
-		int flag = 0;
-		
-		
-		return flag;
+		log.info("s_update()....");
+		log.info(vo.toString());
+
+		return sqlSession.update("S_UPDATE", vo);
 	}
 
 	@Override
 	public int s_delete(StoryVO vo) {
-		System.out.println("delete()....");
-		System.out.println(vo);
-		int flag = 0;
-		
-		return flag;
+		log.info("s_delete()....");
+		log.info(vo.toString());
+
+		return sqlSession.delete("S_DELETE", vo);
 	}
 	
 	@Override
 	public List<StoryVO> s_selectRandomList(StoryVO vo) {
-		System.out.println("s_selectRandomList()....");
-		System.out.println(vo);
-		List<StoryVO> vo2 = new ArrayList<StoryVO>();
-		
-		
-		return vo2;
+		log.info("s_selectRandomList()....");
+		log.info(vo.toString());
+
+		return sqlSession.selectList("S_SELECTRANDOMLIST", vo);
 	}
 
 	@Override
 	public List<StoryVO> s_selectAll() {
-		System.out.println("selectAll_Story()...");
-		List<StoryVO> vos = new ArrayList<StoryVO>();
-		
-		
-		return vos;
+		log.info("s_selectAll()...");
+
+		return sqlSession.selectList("S_SELECT_ALL");
 	}
 
 	@Override
 	public List<StoryVO> s_selectAll(int cpage, int pageBlock) {
-		System.out.println("selectAll_Story(page)....");
-		System.out.println("cpage:" + cpage);
-		System.out.println("pageBlock:" + pageBlock);
-		List<StoryVO> vos = new ArrayList<StoryVO>();
-		
-		return vos;
+		log.info("s_selectAll()....");
+		log.info("cpage:" + cpage);
+		log.info("pageBlock:" + pageBlock);
+
+		int startRow = (cpage - 1) * pageBlock + 1;
+		int endRow = startRow + pageBlock - 1;
+		log.info(startRow + "," + endRow);
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("startRow", startRow);
+		map.put("endRow", endRow);
+
+		return sqlSession.selectList("S_SELECT_ALL_PAGE_BLOCK", map);
 	}
 
 	@Override
@@ -87,24 +89,29 @@ public class StoryDAOimpl implements StoryDAO {
 
 	@Override
 	public int s_removeExpired() {
-		System.out.println("s_removeExpired....");
-		
-		
-		return 0;
+		log.info("s_removeExpired()...");
+
+		return sqlSession.delete("S_REMOVEEXPIRED");
 	}
 
 	@Override
 	public int s_increaseGood(StoryVO vo) {
-		System.out.println("s_increaseGood....");
-		
-		return 0;
+		// 해당 게시물의 추천수를 가져와 1 증가시킴
+	    int currentGoodCount = vo.getGood();
+	    vo.setGood(currentGoodCount + 1);
+
+	    // 데이터베이스에 업데이트
+	    return sqlSession.update("UPDATE_GOOD_COUNT", vo);
 	}
 
 	@Override
 	public int s_increaseReport(StoryVO vo) {
-		System.out.println("s_increaseReport....");
-		
-		return 0;
+		// 해당 게시물의 신고수를 가져와 1 증가시킴
+	    int currentReportCount = vo.getReport();
+	    vo.setReport(currentReportCount + 1);
+
+	    // 데이터베이스에 업데이트
+	    return sqlSession.update("UPDATE_REPORT_COUNT", vo);
 	}
 
 	
--- movieadmin/src/main/java/com/project/movieadmin/story/comments/SCommentsDAO.java
+++ movieadmin/src/main/java/com/project/movieadmin/story/comments/SCommentsDAO.java
@@ -1,6 +1,5 @@
 package com.project.movieadmin.story.comments;
 
-import java.util.Date;
 import java.util.List;
 
 
@@ -14,12 +13,8 @@ public interface SCommentsDAO {
 	
 	public List<SCommentsVO> sc_selectAll();
 	
-	public List<SCommentsVO> sc_selectAll(int cpage,int pageBlock);
+//	public List<SCommentsVO> sc_selectAll(int cpage,int pageBlock);
 	
-	public Date s_getLiveStories(SCommentsVO vo);
-	//만료되지 않은 스토리 조회
-	public int s_removeExpiredComments();
-	//만료된 스토리 댓글 삭제
 	public int s_increaseGood(SCommentsVO vo); 
 	//스토리 좋아요 수 증가
 	
