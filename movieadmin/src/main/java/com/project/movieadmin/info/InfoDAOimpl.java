0c66cddbde27a105874db2b50334b18f00455149
Sejin Hong <ladybug12250@gmail.com>
2024-04-09 17:49:00 +0900

    commit-Info

--- movieadmin/src/main/java/com/project/movieadmin/info/InfoDAOimpl.java
+++ movieadmin/src/main/java/com/project/movieadmin/info/InfoDAOimpl.java
@@ -15,23 +15,68 @@ public class InfoDAOimpl implements InfoDAO {
 	
 	@Autowired
 	private SqlSession sqlSession;
+	
+	public InfoDAOimpl() {
+		log.info("InfoDAOimpl()....");
+	}
 
 	@Override
 	public InfoDAO i_selectOne(InfoVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("selectOne()....");
+		log.info(vo.toString());
+
+		NewsVO vo2 = sqlSession.selectOne("N_SELECT_ONE", vo);
+
+		return vo2;
 	}
 
 	@Override
 	public List<InfoDAO> i_selectAll(int cpage, int pageBlock) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("selectAll()....");
+		log.info("cpage:" + cpage);
+		log.info("pageBlock:" + pageBlock);
+
+		int startRow = (cpage - 1) * pageBlock + 1;
+		
+		log.info("startRow:{}",startRow);
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("startRow", startRow-1);
+		map.put("pageBlock", pageBlock);
+
+		List<InfoVO> vos = sqlSession.selectList("N_SELECT_ALL_PAGE_BLOCK", map);
+
+		return vos;
 	}
 
 	@Override
 	public List<InfoDAO> i_searchList(String searchKey, String searchWord, int cpage, int pageBlock) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("searchList()....");
+		log.info(searchKey);
+		log.info(searchWord);
+
+		log.info("cpage:" + cpage);
+		log.info("pageBlock:" + pageBlock);
+
+		int startRow = (cpage - 1) * pageBlock + 1;
+		log.info("startRow:{}",startRow);
+
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("startRow", startRow-1);
+
+		map.put("pageBlock", pageBlock);
+		map.put("searchWord", "%" + searchWord + "%");
+
+		List<NewsVO> vos = null;
+
+		if (searchKey.equals(" ")) {
+			vos = sqlSession.selectList("I_SEARCHLIST_PAGE_BLOCK_TITLE", map);
+		} else if (searchKey.equals(" ")) {
+			vos = sqlSession.selectList("I_SEARCHLIST_PAGE_BLOCK_CONTENT", map);
+		}
+
+		return vos;
 	}
 
 	@Override
