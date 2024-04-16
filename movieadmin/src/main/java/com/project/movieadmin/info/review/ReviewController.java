addd4ed01f4c87c580d8401d5ce4d1895d587984
Sejin Hong <ladybug12250@gmail.com>
2024-04-16 17:12:00 +0900

    .

--- movieadmin/src/main/java/com/project/movieadmin/info/review/ReviewController.java
+++ movieadmin/src/main/java/com/project/movieadmin/info/review/ReviewController.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.project.movieadmin.info.review.ReviewService;
+import com.project.movieadmin.info.review.ReviewVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +24,15 @@ public class ReviewController {
 	
 	@Autowired
 	ReviewService service;
+	
+	@Autowired
+	private ServletContext sContext;
+
+	@Autowired
+	private HttpSession session;
+	
+	@Autowired
+	private InfoService comService;
 
 	private static final Logger logger = LoggerFactory.getLogger(ReviewController.class);
 	
@@ -30,16 +43,57 @@ public class ReviewController {
 	@RequestMapping(value = "/rv_insert.do", method = RequestMethod.GET)
 	public String rv_insert(ReviewVO vo) {
 		
-	
+		log.info("Welcome i_insert.do....");
+		String nickname = (String) session.getAttribute("user_id");
+		model.addAttribute(nickname);	
 		return "rv_insert";
 	}
 	
 	@RequestMapping(value = "/rv_insertOK.do", method = RequestMethod.GET)
 	public String rv_insertOK(ReviewVO vo) {
 		
-	
-		return "rv_insertOK";
+		log.info("Welcome rv_insertOK.do...");
+		log.info(vo.toString());
+
+		String realPath = sContext.getRealPath("resources/uploadimg");
+		log.info(realPath);
+
+		String originName = vo.getFile_img().getOriginalFilename();
+
+		log.info("getOriginalFilename:{}", originName);
+		
+		if (originName.length() == 0) {
+			vo.setSave_img("default.png");
+		} else {
+			String save_img = "img_" + System.currentTimeMillis() + originName.substring(originName.lastIndexOf("."));
+
+			vo.setSave_img(save_img);
+
+			File uploadFile = new File(realPath, save_img);
+			vo.getFile_img().transferTo(uploadFile);
+
+			//// create thumbnail image/////////
+			BufferedImage original_buffer_img = ImageIO.read(uploadFile);
+			BufferedImage thumb_buffer_img = new BufferedImage(50, 50, BufferedImage.TYPE_3BYTE_BGR);
+			Graphics2D graphic = thumb_buffer_img.createGraphics();
+			graphic.drawImage(original_buffer_img, 0, 0, 50, 50, null);
+
+			File thumb_file = new File(realPath, "thumb_" + save_img);
+
+			ImageIO.write(thumb_buffer_img, save_img.substring(save_img.lastIndexOf(".") + 1), thumb_file);
+
+		}
+
+		int result = service.rv_insert(vo);
+
+		if (result == 1) {
+			return "redirect:rv_selectAll.do";
+		} else {
+			return "redirect:rv_insert.do";
+		}
 	}
+	
+	
 	
 	@RequestMapping(value = "/rv_update.do", method = RequestMethod.GET)
 	public String rv_update(ReviewVO vo) {
